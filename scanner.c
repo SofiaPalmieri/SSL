18a3473fd8a1fcb09ad1f0c483c502bf947237a6
Sofia Palmieri <spalmieri@frba.utn.edu.ar>
2021-08-14 13:04:00 -0300

    Modifico scanner.c

--- scanner.c
+++ scanner.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
 #include "scanner.h"
 
 typedef enum
@@ -8,13 +5,14 @@ typedef enum
     e0_inicial,
     e1_cad,
     e2_sep,
-    e3_fdt
+    e3_esp,
+    e4_fdt
 } Estado;
 
 token getToken(void);
 
-token getToken(void){
-    static estadoActual = e0_inicial;
+token getToken(){
+    static int estadoActual = e0_inicial;
     char caracter;
     while(caracter = getchar() != EOF){
         switch(estadoActual){
@@ -25,12 +23,39 @@ token getToken(void){
             if(caracter == ','){
                estadoActual = e2_sep;
             }
-            if(caracter == "\n"){
-                estadoActual = e3_fdt;
+            if(isspace(caracter)){
+                estadoActual = e3_esp;
+            }
+            if(caracter == '\n'){
+                estadoActual = e4_fdt;
                 caracter = EOF;
                 return fdt;
                 break;
             }
+            case e1_cad:
+            if(isalpha(caracter) | ispunct(caracter) | isdigit(caracter)){
+                estadoActual = e0_inicial;
+                ungetc(caracter, stdin);
+                return cad;
+                break;
+            }
+            case e2_sep:
+            if(caracter == ','){
+                estadoActual = e0_inicial;
+                ungetc(caracter, stdin);
+                return sep;
+                break;
+            }
+            case e3_esp:
+            if(isspace(caracter)){
+                estadoActual = e0_inicial;
+                ungetc(caracter, stdin);
+                return esp;
+                break;
+            }
+            default:
+            estadoActual = fdt;
+            break;
         }
     }
 
