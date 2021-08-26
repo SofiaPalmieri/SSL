e4220ff98602358d38b5681f7bff4d3ded8978e3
Sofia Palmieri <spalmieri@frba.utn.edu.ar>
2021-08-26 01:04:23 -0300

    Algunos cambios

--- scanner.c
+++ scanner.c
@@ -1,7 +1,6 @@
 #include "scanner.h"
 
-typedef enum
-{
+typedef enum {
     e0_inicial,
     e1_cad,
     e2_sep,
@@ -11,53 +10,85 @@ typedef enum
 
 token getToken(void);
 
-token getToken(){
+void imprimirCadena(token);
+
+token getToken()
+{
     static int estadoActual = e0_inicial;
     char caracter;
-    while(caracter = getchar() != EOF){
-        switch(estadoActual){
-            case e0_inicial:
-            if(isalpha(caracter) | ispunct(caracter) | isdigit(caracter)){
+    while ((caracter = getchar()) != EOF)
+    {
+        switch (estadoActual)
+        {
+        case e0_inicial:
+            if (isalpha(caracter) || ispunct(caracter) || isdigit(caracter))
+            {
                 estadoActual = e1_cad;
             }
-            if(caracter == ','){
-               estadoActual = e2_sep;
+            if (caracter == ',')
+            {
+                estadoActual = e2_sep;
             }
-            if(isspace(caracter)){
+            if (isspace(caracter))
+            {
                 estadoActual = e3_esp;
             }
-            if(caracter == '\n'){
+            if (caracter == '\n')
+            {
                 estadoActual = e4_fdt;
                 caracter = EOF;
                 return fdt;
                 break;
             }
-            case e1_cad:
-            if(isalpha(caracter) | ispunct(caracter) | isdigit(caracter)){
+        case e1_cad:
+            if (isalpha(caracter) || ispunct(caracter) || isdigit(caracter))
+            {
                 estadoActual = e0_inicial;
                 ungetc(caracter, stdin);
                 return cad;
                 break;
             }
-            case e2_sep:
-            if(caracter == ','){
+        case e2_sep:
+            if (caracter == ',')
+            {
                 estadoActual = e0_inicial;
                 ungetc(caracter, stdin);
                 return sep;
                 break;
             }
-            case e3_esp:
-            if(isspace(caracter)){
+        case e3_esp:
+            if (isspace(caracter))
+            {
                 estadoActual = e0_inicial;
                 ungetc(caracter, stdin);
                 return esp;
                 break;
             }
-            default:
+        default:
             estadoActual = fdt;
             break;
         }
     }
-
+    return fdt;
 }
 
+void imprimirCadena(token unToken)
+{
+    do
+    {
+        switch (unToken)
+        {
+        case cad:
+            printf("Cadena: %d", cad);
+            break;
+        case sep:
+            printf("Separador: %d", sep);
+            break;
+        case fdt:
+            printf("Fin de Texto: %d", fdt);
+            break;
+        default:
+            break;
+        }
+    } while (unToken != EOF);
+}
