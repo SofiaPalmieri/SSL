0e5b1af45371226416b8f8395f606f210c8f1ea7
Sofia Palmieri <spalmieri@frba.utn.edu.ar>
2021-08-26 01:05:15 -0300

    Algunos cambios

--- main.c
+++ main.c
@@ -1,20 +1,8 @@
 #include "scanner.h"
 
-int main(void){
-    token unToken;
-    unToken = getToken();
-    switch(unToken){
-    case cad:
-    printf("Cadena: %s", cad);
-    break;
-    case sep:
-    printf("Separador: %s", sep);
-    break;
-    case fdt:
-    printf("Fin de Texto: %s, fdt");
-    break;
-    default:
-    break;
-}
-return 0;
+int main(void)
+{
+    token unToken = getToken();
+    imprimirCadena(unToken);
+    return 0;
 }
